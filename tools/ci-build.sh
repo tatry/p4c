7f4945e9ea13fd779bc2ab8b0f6e1755dc19bd1e
Jan Palimąka <25018671+tatry@users.noreply.github.com>
2022-10-21 14:40:22 +0200

    Test https://github.com/NIKSS-vSwitch/nikss/pull/79

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -82,8 +82,9 @@ function install_ptf_ebpf_test_deps() (
                            $LINUX_TOOLS"
   apt-get install -y --no-install-recommends ${P4C_PTF_PACKAGES}
 
-  git clone --depth 1 --recursive --branch v0.2.0 https://github.com/NIKSS-vSwitch/nikss /tmp/nikss
+  git clone --depth 1 --recursive --branch T71 https://github.com/NIKSS-vSwitch/nikss /tmp/nikss
   cd /tmp/nikss
+  git reset --hard 2e747e9
   ./build_libbpf.sh
   mkdir build
   cd build
